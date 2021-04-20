647ae0d3393fe27583f17f2d020354aba3753b3d
clowry22 <clowry@uchicago.edu>
2021-04-20 16:50:03 +0000

    Added indirect associations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,7 +13,10 @@
 #
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User", counter_cache: true
-
+  
   has_many :comments
   has_many :likes
+
+  #indirect associations
+  has_many :fans, through: :likes#, source: :fan
 end
