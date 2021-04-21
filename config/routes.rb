0dccab00b030bcc9a623d8a46aa2676233ccb153
clowry22 <clowry@uchicago.edu>
2021-04-21 01:22:55 +0000

    Built out liked tab

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  get "/:username" => "users#show"
+
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#liked", as: :liked_photos
+  get ":username/followers" => "photos#liked", as: :liked_photos
+  get ":username/following" => "photos#liked", as: :liked_photos
+  get ":username" => "users#show", as: :user
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
